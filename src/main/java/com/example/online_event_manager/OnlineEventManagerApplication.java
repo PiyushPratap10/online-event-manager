554e3f4c56b1c588050e730b8f14a74f59fbc490
PiyushPratap10 <pratappiyush638@gmail.com>
2024-12-28 19:01:49 +0530

    Project Initialized

---
+++ src/main/java/com/example/online_event_manager/OnlineEventManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_event_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEventManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEventManagerApplication.class, args);
+	}
+
+}
